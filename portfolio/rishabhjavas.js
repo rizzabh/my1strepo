f55acb7f5d2d05ad5ad0d11cafa3d7fb1c0e6f05
Rishabh Pandey <133371863+rizzabh@users.noreply.github.com>
2023-05-16 00:34:54 +0530

    Add files via upload
    portfolio

---
+++ portfolio/rishabhjavas.js
@@ -0,0 +1,27 @@
+
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("nav");
+
+
+var sticky = navbar.offsetTop;
+
+ 
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
